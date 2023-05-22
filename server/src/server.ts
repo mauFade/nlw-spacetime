7762fbaea2472ecbd8baeca727d72dccf85e9d20
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-05-22 12:26:18 -0300

    feat: add eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 import fastify from "fastify";
 
 const app = fastify();
+
+app.get("/", () => {
+  return "Fala ai meu mano";
+});
+
+async function main() {
+  const PORT = 4040;
+
+  await app.listen({ port: PORT });
+
+  console.log(`App running at port: ${PORT}`);
+}
+
+main();
